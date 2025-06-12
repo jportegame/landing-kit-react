266c213fe0373b0f57b140e45de4952d5da0bfd3
Juan Pablo Ortega <jportegame@gmail.com>
2025-06-12 00:05:25 -0500

    add: vercel insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+// @ts-ignore
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </StrictMode>,
 )
